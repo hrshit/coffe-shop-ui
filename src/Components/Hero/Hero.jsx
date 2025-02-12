65e0ecdffb9b9f96ab2080fc2316fbb3be4f9fcf
hrshit <chouhanharsh381@gmail.com>
2025-02-12 12:45:45 +0530

    initial commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import BgImage from "../../assets/bg-slate.png"
+
+// const bgImage = {
+//     backgroundImage: `url(${BgImage})`
+//     backgroundSize: 
+// }
+
+const Hero = () => {
+  return (
+    <main>
+          <section>
+              <div className="container">
+                  Navbar section
+              </div>
+          </section>      
+    </main>
+  )
+}
+
+export default Hero
